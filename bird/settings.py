e8242c5a56320072930d468c809f2200c93264e4
vikas <132460296+obsoftteam@users.noreply.github.com>
2023-08-28 15:09:51 +0530

    commit

--- bird/settings.py
+++ bird/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2fa+60!dfpq1*b^@^dwfveq4dyo+_jc8#vvlv5o1672k3=ny=r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
